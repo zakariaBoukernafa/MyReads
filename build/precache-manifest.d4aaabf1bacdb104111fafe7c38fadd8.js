5bfa9f57bc1a0d6b8ef4c313634c021a7da6d3e5
zakariaBoukernafa <zakaria0697@gmail.com>
2020-11-24 15:43:43 +0100

    add to githubpages

---
+++ build/precache-manifest.d4aaabf1bacdb104111fafe7c38fadd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a48618b3d1c29c18495",
+    "url": "/MyReads/static/css/main.39affb9f.chunk.css"
+  },
+  {
+    "revision": "6a48618b3d1c29c18495",
+    "url": "/MyReads/static/js/main.6a48618b.chunk.js"
+  },
+  {
+    "revision": "fd747ffdaf7408adc063",
+    "url": "/MyReads/static/js/1.fd747ffd.chunk.js"
+  },
+  {
+    "revision": "970c0060ff174c587b9c",
+    "url": "/MyReads/static/js/runtime~main.970c0060.js"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/MyReads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/MyReads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/MyReads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "12255850c1418f7b273ccb5805f2fb4c",
+    "url": "/MyReads/index.html"
+  }
+];
